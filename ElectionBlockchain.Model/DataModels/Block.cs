6ae5d301c42f03e47291446d65437a5817fc139e
Dannymane <daniel.yanko5@gmail.com>
2023-06-22 12:43:05 +0200

    Models created

---
+++ ElectionBlockchain.Model/DataModels/Block.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectionBlockchain.Model.DataModels
+{
+   public class Block
+   {
+      public int Id { get; set; }
+      public virtual IList<Vote> Votes { get; set; }
+      public string LeaderSignature { get; set; }
+      public string FirstVerifierSignature { get; set; }
+      public string SecondVerifierSignature { get; set; }
+      public string PreviousBlockHash { get; set; }
+
+      public string Hash { get; set; }
+   }
+}
