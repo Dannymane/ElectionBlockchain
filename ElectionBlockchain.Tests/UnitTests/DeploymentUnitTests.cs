79d35cd477a4c625a627a521043191802a7ee19a
Dannymane <daniel.yanko5@gmail.com>
2023-12-03 20:25:40 +0100

    unit tests

--- ElectionBlockchain.Tests/UnitTests/DeploymentUnitTests.cs
+++ ElectionBlockchain.Tests/UnitTests/DeploymentUnitTests.cs
@@ -4,10 +4,11 @@ using ElectionBlockchain.Services.ConcreteServices;
 using ElectionBlockchain.Services.Interfaces;
 using ElectionBlockchain.DAL.EF;
 using Newtonsoft.Json;
+using Microsoft.EntityFrameworkCore;
 
 namespace ElectionBlockchain.Tests.UnitTests
 {
-    public class DeploymentUnitTests : BaseUnitTests
+    public class DeploymentUnitTests : BaseUnitTests, IDisposable
     {
       public DeploymentUnitTests(ApplicationDbContext dbContext, IDatabaseService databaseService,
          ILeaderService leaderService, IVerifierService verifierService)
@@ -28,5 +29,31 @@ namespace ElectionBlockchain.Tests.UnitTests
          Assert.Equal(24, citizens?.Count());
          Assert.Equal("DOC010", citizens?[0].CitizenDocumentId);
       }
-    }
+
+      [Fact]
+      public async Task AddCandidatesAsync()
+      {
+         IEnumerable<Candidate> candidates = new List<Candidate> {
+            new Candidate() {Name = "Daniel", Surname = "Yanko" },
+            new Candidate() {Name = "John", Surname = "Miller" }
+         };
+
+         await DatabaseService.AddCandidatesAsync(candidates);
+         List<Candidate> addedCandidates = await DbContext.Candidates.ToListAsync();
+         Assert.Equal(2, addedCandidates?.Count());
+         Assert.Equal("Daniel", addedCandidates?[0].Name);
+         Assert.Equal("John", addedCandidates?[1].Name);
+
+      }
+
+      public void Dispose()
+      {
+         DatabaseService.Clean("blocks");
+         DatabaseService.Clean("votes");
+         DatabaseService.Clean("votesqueue");
+         DatabaseService.Clean("citizens");
+         DatabaseService.Clean("citizenprivatekeys");
+         DatabaseService.Clean("candidates");
+      }
+   }
 }
